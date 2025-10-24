ecb641551dff0373ba0116b5ce2dd925f219c1c9
vloliveira <vitorloliveira96@gmail.com>
2025-10-24 14:53:26 -0300

    feat: challenge remover dupluicata em TypeScript

--- day5/TS/removerDuplicata.ts
+++ day5/TS/removerDuplicata.ts
@@ -1,2 +1,31 @@
-console.log("Olá");
-console.log("Olá");
+import * as readline from "readline/promises";
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const lista: number[] = [];
+const lista2: number[] = [];
+
+async function receberNum() {
+  for (let i = 0; i < 6; i++) {
+    const resposta: string = await rl.question(`Digite o ${i + 1} número: `);
+    const num = Number(resposta);
+    lista.push(num);
+
+    let count = 0;
+    for (let y = 0; y < lista2.length; y++) {
+      if (num == lista2[y]) {
+        count++;
+      }
+    }
+    if (count == 0) {
+      lista2.push(num);
+    }
+  }
+  rl.close;
+  console.log(lista);
+  console.log(lista2);
+}
+
+receberNum();
